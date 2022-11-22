83f4d3e4ce1d99a327d401e6dfe3bfad8db09626
Rrytel <89410978+Rrytel@users.noreply.github.com>
2022-11-22 15:54:02 -0800

    fix particles

--- Food Dash/Assets/stove_controller.cs
+++ Food Dash/Assets/stove_controller.cs
@@ -6,22 +6,28 @@ public class stove_controller : MonoBehaviour
 {
 
     ParticleSystem ps;
+    float angle;
     // Start is called before the first frame update
     void Start()
     {
          ps = GetComponent<ParticleSystem>();
+         angle = 0;
     }
 
     // Update is called once per frame
     void Update()
     {
         var emission = ps.emission;
+        
         if (Input.GetKeyDown(KeyCode.Space))
         {
             // Spacebar was pressed 
             Debug.Log("Hello: ");
             //emission.enabled = false;
             ps.Stop(true);
+            angle += 15;
         }
+        //GetComponentInChildren<Transform>().eulerAngles = new Vector3(0f, angle, 0f);
+        transform.Find("pan").eulerAngles = new Vector3(0f, angle, 0f);
     }
 }
