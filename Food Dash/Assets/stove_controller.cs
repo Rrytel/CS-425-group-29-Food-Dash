186c9b881e389d3e66939287f8790cf43f0a01a8
Rrytel <89410978+Rrytel@users.noreply.github.com>
2022-11-22 15:40:40 -0800

    added textures and particle control

--- Food Dash/Assets/stove_controller.cs
+++ Food Dash/Assets/stove_controller.cs
@@ -4,15 +4,24 @@ using UnityEngine;
 
 public class stove_controller : MonoBehaviour
 {
+
+    ParticleSystem ps;
     // Start is called before the first frame update
     void Start()
     {
-        
+         ps = GetComponent<ParticleSystem>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        var emission = ps.emission;
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            // Spacebar was pressed 
+            Debug.Log("Hello: ");
+            //emission.enabled = false;
+            ps.Stop(true);
+        }
     }
 }
