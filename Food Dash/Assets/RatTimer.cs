dae3f8926dd58de7644f5bcba02e718b4c62bf4a
Compton-Kiara <kdcmpton23@nevada.unr.edu>
2023-02-04 14:35:08 -0800

    added spawning holes

--- Food Dash/Assets/RatTimer.cs
+++ Food Dash/Assets/RatTimer.cs
@@ -5,11 +5,12 @@ using UnityEngine;
 public class RatTimer : MonoBehaviour
 {
     public GameObject ratObject;
-    public float spawnTime = 45;
+    public float spawnTime = 15;
+    public List<GameObject> spawns;
     // Start is called before the first frame update
     void Start()
     {
-        
+        spawnTime = 1;
     }
 
     // Update is called once per frame
@@ -20,13 +21,22 @@ public class RatTimer : MonoBehaviour
         if (spawnTime < 0)
         {
             //reset the time to 45 sec
-            spawnTime = 45f;
-
+            spawnTime = 1f;
+            
+            int index = Random.Range(0, spawns.Count);
+            Debug.Log("index: "+index);
             //give the rat a random pos.
-            var randPos = new Vector3(Random.Range(-4.96f, 0.04f), 0.19f, Random.Range(-3.18f, -0.7f));
+            var randPos = spawns[index].transform.position;
+            //var randPos = new Vector3(Random.Range(-4.96f, 0.04f), 0.19f, Random.Range(-3.18f, -0.7f));
             //create an object at the random positon calculated
             Instantiate (ratObject, randPos, Quaternion.identity);
-            Debug.Log(spawnTime);
+            //Debug.Log(spawnTime);
+            Debug.Log("List Size: " + spawns.Count.ToString());
+
+            for(int i =0; i < spawns.Count; i++)
+            {
+                Debug.Log (spawns[i]);
+            }
 
             //spawnTime = 45f;
             
