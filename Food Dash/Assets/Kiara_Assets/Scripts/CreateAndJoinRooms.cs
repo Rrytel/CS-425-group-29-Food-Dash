97473cc2d92a7d1c86aa12d4f9b294fb1ad225a7
Compton-Kiara <kdcmpton23@nevada.unr.edu>
2023-02-27 04:20:06 -0800

    added wait room so players will sync up in joining game, still issues with object flying up

--- Food Dash/Assets/Kiara_Assets/Scripts/CreateAndJoinRooms.cs
+++ Food Dash/Assets/Kiara_Assets/Scripts/CreateAndJoinRooms.cs
@@ -10,6 +10,8 @@ public class CreateAndJoinRooms : MonoBehaviourPunCallbacks
 {
     public InputField createInput;
     public InputField joinInput;
+    //checks if all players are connected before syncing them up and sending them to the created room
+    bool allConnected = false;
 
     //Have the timer run only on the host, ev. time when timer lapses send out a network message to all game clients, that notifies it and then - ASK RYAN about his timer script - have to put photon into his script
     //RPC's - PHOTON
@@ -28,11 +30,13 @@ public class CreateAndJoinRooms : MonoBehaviourPunCallbacks
         RoomOptions roomOptions = new RoomOptions();
         roomOptions.MaxPlayers = 2;
 
+        
+
         //Note: ADD where to make sure input field isn't empty before creating room
         //need string input for room name-> name of room will be whatever we write in the "createInput" input field
-        PhotonNetwork.CreateRoom(createInput.text, roomOptions);
+        PhotonNetwork.CreateRoom(createInput.text, roomOptions); //automatically calls OnJoinedRoom
     }
-
+    //if client has successfully connected to the room
     public void JoinRoom()
     {
         //Joining the room of whatever is in the joinINput text field
@@ -42,7 +46,32 @@ public class CreateAndJoinRooms : MonoBehaviourPunCallbacks
      //using callback fcn that's automatically called once joined a room
      public override void OnJoinedRoom()
     {
+        //send the master to waiting for others scene - then check how many players are in the scene- if 2, then load the game scene with both of them in it
+        PhotonNetwork.LoadLevel("WaitingForPlayer");
+        allConnected = checkIfAllPlayers();
         //Transition to actual game scene
-        PhotonNetwork.LoadLevel("food_dash_Kiara");
+        if (allConnected)
+        {
+            PhotonNetwork.LoadLevel("food_dash_Kiara");
+        }
+        else
+        {
+            Debug.Log("Sorry not enough players");
+        }
+       
+    }
+
+    bool checkIfAllPlayers()
+    {
+        if (PhotonNetwork.CurrentRoom.PlayerCount == 2)
+        {
+            Debug.Log("All players are in the room!");
+        }
+        else
+        {
+            Debug.Log("Not enough players in the room: " + PhotonNetwork.CurrentRoom.PlayerCount);
+        }
+        return false;
+
     }
 }
