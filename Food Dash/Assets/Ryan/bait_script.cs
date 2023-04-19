a1a7b6ba0a802bc670fe9f923ff4881e2ff882d5
Rrytel <89410978+Rrytel@users.noreply.github.com>
2023-04-19 16:35:38 -0700

    adjust crates

--- Food Dash/Assets/Ryan/bait_script.cs
+++ Food Dash/Assets/Ryan/bait_script.cs
@@ -6,11 +6,13 @@ public class bait_script : MonoBehaviour
 {
     GameObject[] rats;
     GameObject baitController;
+    float timer;
     // Start is called before the first frame update
     void Start()
     {
         baitController = GameObject.FindGameObjectWithTag("BaitController");
         baitController.GetComponent<bait_controller>().AddBait(gameObject);
+	  timer = 10f;
     }
 
     private void Awake()
@@ -33,6 +35,12 @@ public class bait_script : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+	  timer -= .5f * Time.deltaTime;
+        if(timer < 0)
+	  {
+		//Play animation and destroy
+		Destroy(gameObject);
+        }
         /*
         rats = GameObject.FindGameObjectsWithTag("Rat");
         foreach (GameObject rat in rats)
