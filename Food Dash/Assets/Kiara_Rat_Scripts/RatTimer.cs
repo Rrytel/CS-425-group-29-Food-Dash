c22e801d76f07c0a9ca3fb583067d296b2be3c53
Compton-Kiara <kdcmpton23@nevada.unr.edu>
2023-05-01 18:46:59 -0700

    additional instructions, updated rat timer and inspectorscript, updated rat spawn object, changed sized of ingredients
    - updated rat timer script so that it won't spawn until 1 min has passed in the game
    - updated inspector that has same conditions for tutorial level
    - testing if crates highlight when user in trigger area
    - more instructions in tutorial level for rat holes
    - increased size of the ingredients

--- Food Dash/Assets/Kiara_Rat_Scripts/RatTimer.cs
+++ Food Dash/Assets/Kiara_Rat_Scripts/RatTimer.cs
@@ -5,19 +5,25 @@ using UnityEngine;
 public class RatTimer : MonoBehaviour
 {
     public GameObject ratObject;
-    float spawnTime = 15;
-    float gameSpawn = 5;
+    float spawnTime;
+    float gameSpawn;
     public List<GameObject> spawns;
     public GameObject gameController;
+
+    //delay for timer to start for rats to start spawning
+    private float delay = 45.0f; 
+    float timer;
+
     // Start is called before the first frame update
     void Start()
     {
 
         spawnTime = 15;
 
+
         //Based on the round the user is in, spawn a diff. # of rats
         int day = gameController.GetComponent<Round>().day;
-        if (day == 1)
+        if (day == 1 || day == 0)
         {
             gameSpawn = 3;
         }else if(day == 2){
@@ -25,6 +31,8 @@ public class RatTimer : MonoBehaviour
         }else if (day == 3)
         {
             gameSpawn = 7;
+        } else {
+            gameSpawn = day + 4;
         }
         //gameSpawn = 5;
     }
@@ -33,44 +41,50 @@ public class RatTimer : MonoBehaviour
     //Every frame it will subtract the duration of the previous frame(delta time) from am.
     void Update()
     {
-        spawnTime = spawnTime - Time.deltaTime;
-        if (spawnTime <= 0)
+
+        //rats won't spawn until 45 sec has passed in the game and then every 15 sec for ever many rats are to spawn that day
+        timer += Time.deltaTime;
+        if(timer > delay)
         {
-            Debug.Log("This is the spawn time: " + spawnTime);
-            Debug.Log("Time has passed game playing: " + Time.time);
-            //reset the time to 45 sec
-            spawnTime = 15f;
-
-            int index = Random.Range(0, spawns.Count);
-            Debug.Log($"index: {index}");
-            //give the rat a random pos.
-            var randPos = spawns[index].transform.position; 
-            //var randPos = new Vector3(Random.Range(-4.96f, 0.04f), 0.19f, Random.Range(-3.18f, -0.7f));
-            //create an object at the random positon calculated
-            if(gameSpawn > 0)
+                spawnTime = spawnTime - Time.deltaTime;
+            if (spawnTime <= 0)
             {
-               
-               Instantiate(ratObject, randPos, Quaternion.identity);
-                
-                gameSpawn--;
-            }
-            //Instantiate(ratObject, randPos, Quaternion.identity);
-            //Debug.Log(spawnTime);
-            Debug.Log("List Size: " + spawns.Count.ToString());
+                Debug.Log("This is the spawn time: " + spawnTime);
+                Debug.Log("Time has passed game playing: " + Time.time);
+                //reset the time to 15 sec
+                spawnTime = 15f;
 
-            for (int i = 0; i < spawns.Count; i++)
+                int index = Random.Range(0, spawns.Count);
+                Debug.Log($"index: {index}");
+                //give the rat a random pos.
+                var randPos = spawns[index].transform.position; 
+
+                //create an object at the random positon calculated
+                if(gameSpawn > 0)
+                {
+                
+                Instantiate(ratObject, randPos, Quaternion.identity);
+                    
+                    gameSpawn--;
+                }
+                //Debug.Log(spawnTime);
+                Debug.Log("List Size: " + spawns.Count.ToString());
+
+                for (int i = 0; i < spawns.Count; i++)
+                {
+                    Debug.Log(spawns[i]);
+                    
+                }
+                //for (int j=0)
+                //spawnTime = 45f;
+
+            }
+            if (spawnTime == 0)
             {
-                Debug.Log(spawns[i]);
-                
+                Debug.Log("Timer has run out!");
             }
-            //for (int j=0)
-            //spawnTime = 45f;
-
         }
-        if (spawnTime == 0)
-        {
-            Debug.Log("Timer has run out!");
-        }
+        
 
         //Debug.Log("Time has passed game playing: " + Time.time);
     }
